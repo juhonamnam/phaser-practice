6e0e6cb27d6e5d91919818ad54230c60c499a3fb
juhonamnam <jhnamnam@gmail.com>
2023-04-30 16:59:15 +0900

    add loading scene

--- src/acrobaticEvasion/loadingScene.ts
+++ src/acrobaticEvasion/loadingScene.ts
@@ -5,9 +5,42 @@ export class LoadingScene extends Phaser.Scene {
     super("LoadingScene");
   }
 
-  preload() {}
+  preload() {
+    this.load.image("background", "static/acrobaticEvasion/space.png");
+  }
 
-  create() {}
+  create() {
+    const { x, y, width, height } = this.cameras.main;
+    this.add
+      .tileSprite(x, y, width, height, "background")
+      .setOrigin(0)
+      .setScrollFactor(0, 1);
+
+    const center = {
+      x: x + width / 2,
+      y: y + height / 2,
+    };
+
+    this.add
+      .text(center.x, height / 4, "Acrobatic\nEvasion")
+      .setFill("#fff")
+      .setFontSize(50)
+      .setOrigin(0.5)
+      .setDepth(999)
+      .setAlign("center");
+
+    this.add
+      .text(center.x, (height * 3) / 4, "Click to start")
+      .setFill("#fff")
+      .setFontSize(30)
+      .setOrigin(0.5)
+      .setDepth(999)
+      .setAlign("center");
+
+    this.input.once("pointerdown", () => {
+      this.scene.transition({ target: "RoundScene", duration: 500 });
+    });
+  }
 
   update() {}
 }
